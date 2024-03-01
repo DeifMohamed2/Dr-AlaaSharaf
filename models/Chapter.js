10e6fc09f1e01898f0b3eca1585e0e3651896eb7
Deif Mohamed <102861388+DeifMohamed2@users.noreply.github.com>
2024-03-01 21:47:23 +0200

    Add files via upload

---
+++ models/Chapter.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+const { required } = require('nodemon/lib/config')
+const Schema = mongoose.Schema
+
+
+const Chapterschema = new Schema({
+
+
+    chapterName: {
+        type: String, 
+        required: true, 
+    },
+    chapterGrade :{
+        type: String, 
+        required: true,  
+    },
+    chapterAccessibility :{
+        type: String, 
+        required: true,  
+    },
+    chapterPrice: {
+        type: Number, 
+        required: true, 
+    },
+    chapterLectures :{
+        type:Array
+    },
+    chapterSummaries :{
+        type:Array
+    },
+    chapterSolvings:{
+        type:Array
+    },
+
+
+},{timestamps:true});
+
+const Chapter = mongoose.model('Chapter',Chapterschema)
+
+module.exports=Chapter;
